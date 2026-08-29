40e1f37364bdb3b4311adb9219add62997e0db08
Nebras777 <109689970+Nebras777@users.noreply.github.com>
2026-08-29 14:55:59 +1000

    Set up and connect Firebase datase

---
+++ konekt/services/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxkPujRZ4MSAWp2uG2DYb8QgD9d9OFgbI",
+  authDomain: "konekt-f7f63.firebaseapp.com",
+  projectId: "konekt-f7f63",
+  storageBucket: "konekt-f7f63.firebasestorage.app",
+  messagingSenderId: "470768657470",
+  appId: "1:470768657470:web:727fcf728807f405861d6f",
+  measurementId: "G-N6BZZBLFGP"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
